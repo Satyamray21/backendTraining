caafdee40aea557194a161759ccdbe556c024933
Satyamray21 <satyamray43@gmail.com>
2025-04-11 16:36:32 +0530

    user model done

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+const UserSchema = mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"please provide username"],
+        lowercase :true,
+        trim:true,
+        unique:true,
+        index:true,
+    },
+    email:{
+        type:String,
+        required:[true,"please provide email "],
+        unique:true
+    },
+    fullName:{
+        type:String,
+        requied :[ true,"Please provide your full name"],
+
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
+        
+    },
+    refreshToken:{
+        type:String,
+    }
+},{timestamps:true})
+
+export const User = mongoose.model("User",UserSchema);
