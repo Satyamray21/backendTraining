8d7f44ccf92b71a6d184d93018a279b0affea38f
Satyamray21 <satyamray43@gmail.com>
2025-04-21 13:48:50 +0530

    change forget working in forntend

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -19,7 +19,7 @@ router.route("/register").post(
     router.route("/login").post(loginUser)
     router.route("/logout").post(verifyJwt,loggedOut)
     router.route("/refresh-token").post(refreshAccessToken)
-    router.route("/changePassword").post(verifyJwt,changePassword)
+    router.route("/changePassword").post(changePassword)
     router.route("/deleteUserByEmail").delete(deleteByEmail);
     router.route("/getAllUser").get(verifyJwt,findAllUser);
     router.route("/send-reset-code").post(sentResetCode);
